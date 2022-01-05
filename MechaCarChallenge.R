56228902ef65711ca401e8fd83d3f84845a4c6f3
jessezimm <jessezimm@gmail.com>
2022-01-04 21:57:58 -0500

    challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,6 +13,6 @@ lot_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>% summarise(Mea
 
 # Deliverable 3
 t.test(suspension_data$PSI, mu=1500)
-t.test(subset(suspension_data, Manufacturing_lot == "Lot1")$PSI, mu= 1500)
-t.test(subset(suspension_data, Manufacturing_lot == "Lot2")$PSI, mu= 1500)
-t.test(subset(suspension_data, Manufacturing_lot == "Lot3")$PSI, mu= 1500)
+t.test(subset(suspension_data, Manufacturing_Lot == "Lot1")$PSI, mu= 1500)
+t.test(subset(suspension_data, Manufacturing_Lot == "Lot2")$PSI, mu= 1500)
+t.test(subset(suspension_data, Manufacturing_Lot == "Lot3")$PSI, mu= 1500)
